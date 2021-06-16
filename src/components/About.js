37a858fc3ee513ac0a7280bbd572854ea592990b
Vllaadd <vlladdiimiir@gmail.com>
2021-06-16 04:29:39 -0400

    added some style to about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,13 @@ const About = () => {
     return (
         <div className="container about-container">
             <div className='row about-card'>
-                <div className='col-sm'>
+                <div className='col-sm about-col'>
                     Yo ,what's up! 
                     I am trying to become a strong coder. One day at a time and make sure you enjoy what you do, otherwise you'll get lost! I know I can do this!
                 </div>
-                <img src={moi} alt="My caricature"></img>
+                <div className='col-sm'>
+                    <img src={moi} alt="My caricature"></img>
+                </div>
             </div>
         </div>
     )
